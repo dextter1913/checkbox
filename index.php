c90c6d2772a03be1b48ac6f5c51e6f7108fe19fd
dextter1913 <dextter1913@gmail.com>
2021-05-12 10:59:58 -0500

    Revisando funcionamiento de checkbox para aplicarlo en tablas

---
+++ index.php
@@ -0,0 +1,4 @@
+<form action="recibido.php" method="post">
+    <input type="checkbox" name="chkvalidar" id="1">
+    <input type="submit" value="Enviar">
+</form>
---
+++ recibido.php
@@ -0,0 +1,10 @@
+<?php 
+$chkvalidar = $_POST['chkvalidar'];
+    if ($chkvalidar == true) {
+        var_dump($chkvalidar);
+    }
+    if ($chkvalidar == false) {
+        var_dump($chkvalidar);
+    }
+
+?>
